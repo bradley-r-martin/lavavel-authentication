0ac27fd07d4b56b98a18ce952f204cfb7ece5631
Brad Martin <bradmartin@brads-macbook-2.local>
2020-02-16 21:28:42 +1100

    Update

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ $endpoints = [
 ];
 
 
-Route::match(['options'], 'api/1.0/authentication', 'Pursuit\Authentication\app\Controllers\Authentication@options');
-Route::match(['put'], 'api/1.0/authentication', 'Pursuit\Authentication\app\Controllers\Authentication@authenticate');
-Route::delete('api/1.0/authentication', 'Pursuit\Authentication\app\Controllers\Authentication@destroy');
-Route::resource('api/1.0/authentication', 'Pursuit\Authentication\app\Controllers\Authentication', $endpoints);
+Route::match(['options'], 'api/1.0/authentication', 'BRM\Authentication\app\Controllers\Authentication@options');
+Route::match(['put'], 'api/1.0/authentication', 'BRM\Authentication\app\Controllers\Authentication@authenticate');
+Route::delete('api/1.0/authentication', 'BRM\Authentication\app\Controllers\Authentication@destroy');
+Route::resource('api/1.0/authentication', 'BRM\Authentication\app\Controllers\Authentication', $endpoints);
