6c1687bc49c9358945b6781f7c3342dca0848c59
Brad Martin <bradmartin@brads-macbook-2.local>
2020-03-25 12:54:23 +1100

    Init

--- app/Services/Authentication.php
+++ app/Services/Authentication.php
@@ -149,6 +149,7 @@ class Authentication
         if (empty($credentials->series)) {
             $credentials->series = md5(bcrypt(rand()));
         }
+        $credentials->claims['series'] = $credentials->series;
         $credentials->save();
         \Auth::login($credentials);
         return [
@@ -180,6 +181,9 @@ class Authentication
                 try {
                     $token = \JWTAuth::manager()->decode(new \Tymon\JWTAuth\Token($data['token']));
                     $credentials = (new $this->model)->find($token['sub']);
+                    if($credentials->series !==   $token['claims']['series']){
+                      $validator->errors()->add('token', 'Token is Invalid');
+                    }
                 } catch (Exception $e) {
                     if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                         $validator->errors()->add('token', 'Token is Invalid');
